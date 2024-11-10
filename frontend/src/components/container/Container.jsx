588686cf6c53bdc59998bcaafd277ef51e3ff1bd
aasis-17 <167857510+aasis-17@users.noreply.github.com>
2024-11-10 15:14:36 +0545

    Delete frontend directory

--- frontend/src/components/container/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Container({children, className}) {
-  return (
-    <div className= {`${className} max-w-screen-2xl mx-auto h-screen bg-gray-700 `}  >{children}</div>
-  )
-}
-
-export default Container
--- frontend/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import useTimeConverterHook from "./usetimeConverterHook"
-import useDebounce from "./useDebounce"
-import useDataFetch from "./useDataFetch"
-
-export {
-    useTimeConverterHook,
-    useDebounce,
-    useDataFetch
-}
